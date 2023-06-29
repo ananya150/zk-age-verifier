f70653f4b22fdedebd1d8615d92d8e2469e88ec6
ananya150 <akblockchain15@gmail.com>
2023-06-29 20:43:36 +0530

    feat: updated nextauth url

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -1,10 +1,19 @@
 import { MoralisNextApi } from '@moralisweb3/next';
 
+const getUrl = () => {
+  if(process.env.VERCEL_URL){
+    return `https://${process.env.VERCEL_URL}`
+  }
+    return process.env.NEXTAUTH_URL;
+}
+
+const url = getUrl();
+
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY || '',
   authentication: {
-    domain: process.env.NEXTAUTH_URL ? new URL(process.env.NEXTAUTH_URL).host : '',
-    uri: process.env.NEXTAUTH_URL || '',
+    domain: url ? new URL(url).host : '',
+    uri: url || '',
     timeout: 120,
   },
 });
